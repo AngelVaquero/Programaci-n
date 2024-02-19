f6aae4355c92de207f861cabfbb4e37b02db537d
Angel Vaquero <legnafitpro@hotmail.com>
2024-02-19 22:05:39 +0100

    Commit Inicial

---
+++ src (Código fuente mi app)/JAVASCRIPT/01-Tipos/01-Variables.js
@@ -0,0 +1,9 @@
+let NombreVariable = "Hola a todos.";
+let NombreCompleto;
+let nombreCompleto;
+let nombre_completo;
+console.log(NombreVariable); 
+
+let ApellidoVariable, AnimalVariable;
+ApellidoVariable = 'Vaquero';
+AnimalVariable = 'Perro';
